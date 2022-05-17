95b34dd929bcbf35acda59f28bd859752f458217
James <jemsgold21@gmail.com>
2022-05-16 22:30:50 -0500

    another

---
+++ 8 kyu/Training JS #4 Basic data types--Array.js
@@ -0,0 +1,23 @@
+function getLength(arr){
+    //return length of arr
+    return ?
+  }
+  function getFirst(arr){
+    //return the first element of arr
+    return ?
+  }
+  function getLast(arr){
+    //return the last element of arr
+    return ?
+  }
+  function pushElement(arr){
+    var el=1;
+    //push el to arr
+    
+    return arr
+  }
+  function popElement(arr){
+    //pop an element from arr
+    
+    return ?
+  }
