af122dd52f3f690b46a56d729dc2272cb02974c1
James <jemsgold21@gmail.com>
2022-05-21 21:06:00 -0500

    another

---
+++ 8 kyu/Sum of differences in array.js
@@ -0,0 +1,8 @@
+function sumOfDifferences(arr) {
+    let res = 0
+    let sorted = arr.sort((a,b) => b-a)
+    for (let i = 1; i< sorted.length; i++) {
+      res += (sorted[i-1] - sorted[i])
+    }
+    return res
+  }
