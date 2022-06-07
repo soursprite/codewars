12899d5ffcf0e45c96d51b66d4ee46d2974d1dbe
James <jemsgold21@gmail.com>
2022-06-06 20:21:39 -0500

    8

--- 8 kyu/101 Dalmatians - squash the bugs, not the dogs.js
+++ 8 kyu/101 Dalmatians - squash the bugs, not the dogs.js
@@ -5,5 +5,5 @@ let howManyDalmatians = function(number) {
     var respond = number <= 10 ? dogs[0] : (number <= 50 ? dogs[1] : (number == 101  ? dogs[3] : dogs[2]))
     
   return respond
-    }
+     }
   
