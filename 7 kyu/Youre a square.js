7fa132280a3c9844bafc2d369921dab5529ff8d5
James <bumboy@gmail.com>
2022-11-02 20:00:00 -1100

    7

---
+++ 7 kyu/Youre a square.js
@@ -0,0 +1,8 @@
+var isSquare = function(n){
+    if ( n > 3 ) {
+      for ( let i = 2; i <= n/2; i++) {
+        if (i === n/i) {return true}
+      }
+    } else if (n === 0 || n=== 1) {return true}
+    return false
+  }
